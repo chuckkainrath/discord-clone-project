87219e91c31ecf62f754ae5756c3474f020da8ec
Joemanf <joemanf0@gmail.com>
2021-04-26 19:19:19 -0400

    Adding servers added

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -22,8 +22,8 @@ const NavBar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
+          <NavLink to="/servers" exact={true} activeClassName="active">
+            Servers
           </NavLink>
         </li>
         <li>
---
+++ react-app/src/components/server/ServerNavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServerNavBar() {
+    return (
+        <>
+            {/* <div>Hi</div> */}
+        </>
+    )
+}
+
+export default ServerNavBar
