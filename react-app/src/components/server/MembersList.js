3cd155517630fefef04a11239c44a336f94c8839
Joemanf <joemanf0@gmail.com>
2021-04-29 19:02:21 -0400

    Added ability to view all members in a server (maybe?)?

--- react-app/src/components/server/MembersList.js
+++ react-app/src/components/server/MembersList.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useServer } from '../../context/ServerContext'
+import { getUsersForSidebar } from '../../store/members'
 
 function MembersList() {
+    const dispatch = useDispatch()
+
+    const { serverId } = useServer()
+
+    const members = useSelector(state => state.members)
+
+    const membersArr = []
+
+    for (let member in members) {
+        membersArr.push(members[member])
+    }
+
+    console.log('PLEASE GOD, ', membersArr)
+
+    useEffect(() => {
+        dispatch(getUsersForSidebar(serverId))
+    }, [serverId])
+
     return (
-        <>
-            <div>Members List</div>
-        </>
+        <div>
+            <p>Members:</p>
+            <div>{membersArr.map(member => (
+                member
+            ))}</div>
+        </div>
     )
 }
 
