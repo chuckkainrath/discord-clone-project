d711034b95d92c835f76080bbc77899be8842c36
Malik Moss <malikmoss33@gmail.com>
2021-04-29 15:37:36 -0500

    validated messages not empty

--- react-app/src/components/message/Message.js
+++ react-app/src/components/message/Message.js
@@ -10,6 +10,7 @@ function Message() {
     const dispatch = useDispatch();
     const [chatInput, setChatInput] = useState("");
     const [messages, setMessages] = useState([]);
+    const [chatValid, toggleChatValid] = useState(true)
     const { channelId } = useChannel();
 
     const user = useSelector(state => state.session.user)
@@ -38,6 +39,12 @@ function Message() {
 
     const updateChatInput = (e) => {
         setChatInput(e.target.value)
+        if (e.target.value.length) {
+            toggleChatValid(false)
+
+        } else {
+            toggleChatValid(true)
+        }
     };
 
     const sendChat = (e) => {
@@ -49,6 +56,7 @@ function Message() {
                                      serverId,            // Server message is in
                                      channelId });        // Channel message is in
         setChatInput("")
+        toggleChatValid(true)
     }
 
     return (user && (
@@ -63,7 +71,7 @@ function Message() {
                     value={chatInput}
                     onChange={updateChatInput}
                 />
-                <button type="submit">Send</button>
+                <button type="submit" disabled = {chatValid}>Send</button>
             </form>
         </>)
     )
