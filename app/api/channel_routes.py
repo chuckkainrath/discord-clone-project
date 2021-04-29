86e21bf0cde51fde2c620ab8e02f2ccd8cc380fe
Chuck Kainrath <chuckkainrath@gmail.com>
2021-04-29 12:03:23 -0500

    fixed channel api route issue

--- app/api/channel_routes.py
+++ app/api/channel_routes.py
@@ -9,7 +9,7 @@ channel_routes = Blueprint('channels', __name__)
 @channel_routes.route('/')
 @login_required
 def get_channels(server_id):
-    raw_channels = Channel.query.filter(Server.id == server_id).all()
+    raw_channels = Channel.query.filter(Channel.server_id == server_id).all()
     channels = [channel.to_dict() for channel in raw_channels]
     return {'channels': channels}
 
