78c6056b361f9750531e5e432b98ec8d5c7dafd5
Chuck Kainrath <chuckkainrath@gmail.com>
2021-04-30 13:14:46 -0500

    can accept/decline invites

---
+++ react-app/src/components/server/InviteItem.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useServer } from '../../context/ServerContext';
+import { processInvite } from '../../store/invites';
+import { addServerAction } from '../../store/server';
+
+function InviteItem({invite}) {
+    const dispatch = useDispatch();
+    const { serverId } = useServer();
+
+
+    const processInv = async (acceptInv) => {
+        // dispatch processInv
+        const server = await dispatch(processInvite(invite.server_id, acceptInv));
+        // dispatch add server to store if accept
+        if (acceptInv) {
+            dispatch(addServerAction(server));
+        }
+    }
+
+    return (
+        <li>
+            <div>{invite.server_name}</div>
+            <div>
+                <i onClick={() => processInv(true)} class="fal fa-check"></i>
+                <span>    </span>
+                <i onClick={() => processInv(false)} class="fal fa-times"></i>
+            </div>
+        </li>
+    );
+}
+
+export default InviteItem;
