6470797930060ebd96a8ef0ca136425cffba672e
Joemanf <joemanf0@gmail.com>
2021-04-30 17:11:22 -0400

    Fixed the logout redirect

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
-import { Redirect } from 'react-router-dom'
+import { Redirect, useHistory } from 'react-router-dom'
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
+  const history = useHistory()
+  const [logoutTog, setLogoutTog] = useState(false);
+  useEffect(() => {
+    if (logoutTog) {
+      history.push('/')
+    }
+    setLogoutTog(false)
+  }, [logoutTog])
   const onLogout = async (e) => {
+    setLogoutTog(true)
     await dispatch(logout());
   };
 
