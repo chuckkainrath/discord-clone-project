eee69931e3e312e124c612f63682e45abc1031d9
tyler <tylerruby0314@yahoo.com>
2021-05-02 16:48:15 -0500

    Color Styling, Font Styling, Commiting to merge main before push

--- react-app/src/components/server/MessageList.js
+++ react-app/src/components/server/MessageList.js
@@ -26,7 +26,7 @@ function MessageList() {
         <>
             <div
                 className={styles.channel_name}
-            >{channel && `Channel: ${channel.name}, ${channel.id}`}</div>
+            >{channel && `# ${channel.name}`}</div>
             <Message />
         </>
     )
