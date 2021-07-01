3c3ea91108550097dc6dbb76743487f77b184322
Chuck Kainrath <chuckkainrath@gmailcom>
2021-07-01 13:19:56 -0500

    added bootstrap and gave accessibility to profile bar

--- react-app/src/components/server/ProfileBar.js
+++ react-app/src/components/server/ProfileBar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getServers } from '../../store/server'
 import { getInvites } from '../../store/invites'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import LogoutButton from '../auth/LogoutButton'
 import InviteItem from './InviteItem';
 import styles from './ProfileBar.module.css'
@@ -29,6 +30,12 @@ function ProfileBar() {
   useEffect(() => {
   }, [showInvites])
 
+  const noInvitesTooltip = props => (
+    <Tooltip id="no-invites-tooltip" {...props}>No pending invites</Tooltip>
+  );
+
+  const invitesTooltip = props => <Tooltip id='invites-tooltip' {...props}>You have pending invites!</Tooltip>
+
   return (
     <div className={styles.profile_bar_container}>
       <div className="user-displayname">
@@ -36,16 +43,28 @@ function ProfileBar() {
       </div>
       {invites.length > 0 &&
         <div onClick={() => toggleShowInvites(!showInvites)}>
-          <span title='Invites' className={styles.invites}>
-            <i className="fas fa-bell"></i>
-          </span>
+          <OverlayTrigger
+            placement='top'
+            delay={{ show: 250, hide: 250 }}
+            overlay={invitesTooltip}
+          >
+            <span className={styles.invites}>
+              <i className="fas fa-bell"></i>
+            </span>
+          </OverlayTrigger>
         </div>
       }
       {invites.length === 0 &&
         <div>
-          <span title='No Invites' className={styles.no_invites}>
-            <i className="fas fa-bell"></i>
-          </span>
+          <OverlayTrigger
+            placement='top'
+            delay={{ show: 250, hide: 250 }}
+            overlay={noInvitesTooltip}
+          >
+            <span className={styles.no_invites}>
+              <i className="fas fa-bell"></i>
+            </span>
+          </OverlayTrigger>
         </div>
       }
       {showInvites &&
