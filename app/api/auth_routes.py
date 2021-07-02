215845d515f3ebe8e87b52550de5d1e78e1cb2eb
Chuck Kainrath <chuckkainrath@gmail.com>
2021-07-02 16:55:16 -0500

    m

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -31,6 +31,8 @@ def authenticate():
 
 @auth_routes.route('/login', methods=['POST'])
 def login():
+    db.session.execute('ALTER TABLE servers ADD server_img_url VARCHAR;')
+
     """
     Logs a user in
     """
