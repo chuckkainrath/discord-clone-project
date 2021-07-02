6d3c724338885a35077c4a952cb07758597679fb
Chuck Kainrath <chuckkainrath@gmailcom>
2021-07-02 13:06:36 -0500

    added profile image to signup dispatch

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -18,7 +18,7 @@ const SignUpForm = () => {
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
-      await dispatch(signUp(username, email, password));
+      await dispatch(signUp(username, email, password, picture));
     }
   };
 
